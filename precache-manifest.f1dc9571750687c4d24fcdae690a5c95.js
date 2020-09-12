d5ca3f7781446952cabeab9ba1234f84c51d3d01
Antonio Chavez <antonio.2310cv@gmail.com>
2020-09-11 18:51:18 -0600

    Updates

---
+++ precache-manifest.f1dc9571750687c4d24fcdae690a5c95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bbfc6c1826c562383d9421dc54a234",
+    "url": "/rick-morty-chav/index.html"
+  },
+  {
+    "revision": "85a47c17877d97726b40",
+    "url": "/rick-morty-chav/static/css/main.a11317b5.chunk.css"
+  },
+  {
+    "revision": "22944b21f189992c5468",
+    "url": "/rick-morty-chav/static/js/2.3d32ecae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-morty-chav/static/js/2.3d32ecae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a47c17877d97726b40",
+    "url": "/rick-morty-chav/static/js/main.fbd5faff.chunk.js"
+  },
+  {
+    "revision": "260ea44c0d0f13696d79",
+    "url": "/rick-morty-chav/static/js/runtime-main.35aebc78.js"
+  }
+]);
